27721902051937fc98baadd0838a68e481d0bca4
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2024-08-08 12:02:25 +0200

    fonts configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='w-full h-full bg-[#101828]'>
+      <div className='w-full flex items-center justify-between p-4 text-white font-primaryRegular'>
+        <div>Logo</div>
+        <div>Social media handles</div>
+      </div>
+    </div>
   )
 }
+
+export default App
